202f74f4a68781100d7890a08a22cfe8afa4d86e
SugiuraKiwako04 <shanpuguihezi@gmail.com>
2023-07-15 11:26:55 +0900

    [□と○]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,12 +1,6 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
 
-let count = 0
-
-// ボタン要素のonclickハンドラに関数を代入する
+// ボタンをクリックしたときの処理を登録
 plusButton.onclick = function() {
-  // count を更新
-  count += 1
-  // count を表示
-  display.textContent = count
-}
+  // ここにクリック後の処理を書く
